97196585542b2404a1400e03daea49e07ed9c948
arsovRiste <46527849+Icko888@users.noreply.github.com>
2019-03-07 17:39:01 +0100

    Add files via upload

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int i = 0;
+            //while (i< 10)
+            //    {
+            //    i++;
+            //    }
+            //Console.WriteLine("Ajdeeee sega pecatii ?",i);
+            //Console.ReadLine();
+
+            //int[] num = new int[5];
+
+            //num[1] = 23;
+            //num[2] = 15;
+            //num[0] = 8;
+            //num[4] = 29;
+            //num[3] = 103;
+
+            //string[] week = { "monday", "tuesday", "wendsday", "thursday", "friday", "saturday", "sunday" };
+
+            //Console.WriteLine(num[4]);
+            //Console.WriteLine(week[2]);
+            //Console.ReadLine();
+
+            //int i = 0;
+            //int n = int.Parse(Console.ReadLine());
+            //while (i <= n)
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //}
+
+            //Console.ReadLine();
+            //i = n;
+            //while (i >= 0)
+            //{
+            //    Console.WriteLine(i);
+            //    i--;
+            //}
+
+            //Console.ReadLine();
+
+
+            //int n = int.Parse(Console.ReadLine());
+
+            //for(int i = n; i > 0; i--)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //Console.ReadLine();
+
+            //int odd = int.Parse(Console.ReadLine());
+
+            //for(int i = 2;i <= odd; i = i + 2) 
+            //{
+            //    Console.WriteLine(i);
+            //};
+            //Console.ReadLine();
+
+            //int num = int.Parse(Console.ReadLine());
+
+            //for(int i = 0;i<= num; i++)
+            //{
+            //    if(i % 3 != 0 && i % 7 !=0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+            //Console.ReadLine();
+
+            //int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            //string[] words = { "Pero", "Jane", "Dzole", "Stole", "Rade" };
+            //double[] numbers = { 2.15,3.87,4.89,8.15,7.46};
+            //char[] characters = { 'A', 'B', 'C', 'D', 'E' };
+            //bool[] boolean = { true,false,true,true,false};
+
+            //int[] num = new int[5];
+
+            ////int a = int.Parse(Console.ReadLine());
+            ////int b = int.Parse(Console.ReadLine());
+            ////int c = int.Parse(Console.ReadLine());
+            ////int d = int.Parse(Console.ReadLine());
+            ////int e = int.Parse(Console.ReadLine());
+            //int sum = 0;
+            //for(int i = 0; i < num.Length; i++)
+            //{
+            //    num[i] = int.Parse(Console.ReadLine());
+            //}
+            ////num[0] = a;
+            ////num[1] = b;
+            ////num[2] = c;
+            ////num[3] = d;
+            ////num[4] = e;
+            ////int sum = a + b + c + d + e;
+            //Console.WriteLine(sum);
+            //Console.ReadLine();
+
+            string[] names = { "Pero", "Jane", "Dzole", "Stole", "Rade" };
+
+            string[] newNames = { };
+
+
+
+
+
+        }
+    }
+}
